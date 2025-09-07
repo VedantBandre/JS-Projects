67c2bf43b6df0e3e4f9a035c023702309034dca1
VedantBandre <vedantbandre33@gmail.com>
2025-09-07 13:45:09 +0200

    password generator completed

--- js_apps/js_apps/settings.py
+++ js_apps/js_apps/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     
     'home',
     'calculator',
+    'pw_gen',
 ]
 
 MIDDLEWARE = [
--- js_apps/js_apps/urls.py
+++ js_apps/js_apps/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('calculator/', include('calculator.urls')),
+    path('pw_gen/', include('pw_gen.urls')),
 ]
